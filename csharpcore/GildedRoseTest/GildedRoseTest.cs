1141455e7a6588fbe3316648306b8ed71e4c52c2
Stefan Ilic <stefan.ilic.vie@gmail.com>
2019-11-14 18:29:11 +0100

    Ensure quality degrades twice as fast after sell in date has passed

--- csharpcore/GildedRoseTest/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest/GildedRoseTest.cs
@@ -28,5 +28,16 @@ namespace GildedRoseTest
             
             item.SellIn.ShouldBe(3);
         }
+        
+        [Fact]
+        public void SellInPassed_QualityLoweredByTwo()
+        {
+            var item = new Item { Name = "Item", Quality = 3, SellIn = 0 };
+            var gildedRose = new GildedRose.GildedRose(new[] {item});
+            
+            gildedRose.UpdateQuality();
+            
+            item.Quality.ShouldBe(1);
+        }
     }
 }
